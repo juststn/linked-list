0d6c220a51488c776b92cc89835d2dd2e064d285
juststn <42872030+juststn@users.noreply.github.com>
2018-12-11 08:44:50 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+
+  //Place your code here:
+  int num = 100;
+
+  while(num>=0){
+
+    cout<<num<<endl;
+
+    num = num - 1;
+
+  }
+
+  return 0;
+  
+  }
+
+
+
